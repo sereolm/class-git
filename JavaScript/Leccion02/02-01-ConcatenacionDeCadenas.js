041d25a52f9bd3e8b11fc46024736ae6eabc0157
Serena Olmedo <serenadelvalle25@gmail.com>
2025-06-09 09:35:45 -0300

    Agrego la clase 7, creando una carpeta de leccion03

--- JavaScript/Leccion02/02-01-ConcatenacionDeCadenas.js
+++ JavaScript/Leccion02/02-01-ConcatenacionDeCadenas.js
@@ -13,3 +13,22 @@ console.log(juntos);
 
 nombre += apellido;//Tercera concatenacion usando el operador simplificado
 console.log(nombre);
+
+//Hoy ya no se usa var, se utiliza let y const
+let nombre2 ="Pedro";
+console.log(nombre2);
+
+const apellido2 = "Lepes";
+//apellido2 = "Peres"; una constante no puede ser modificada
+console.log(apellido2);
+
+let x, y; //Se pueden crear varias variables dentro de una misma linea
+x = 17, y = 21;//Se puede hacer asignacion de varias variables dentro de la misma linea
+let z = x + y; //Se asigna el valor de la operacion 
+console.log(z);
+
+let _1num = 31;//NO utilizar numeros para inciar el nombre de una variable
+let rompiendo ="rompe";//NO utilizar palabras reservadas para variables
+
+console.log(_1num);
+console.log(rompiendo);
