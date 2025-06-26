410e88d7f13ca7bfed697e7471545004caf3c19a
Serena Olmedo <serenadelvalle25@gmail.com>
2025-06-26 09:16:05 -0300

    Realice los ejercicios de la clase 8, agregando las capturas finales

--- Python/Leccion3/main.py
+++ Python/Leccion3/main.py
@@ -4,38 +4,40 @@ while contador < 78:
     print('Ejecutamos nuestro ciclo while ' , contador)
     contador += 1
 else:
-    print('Fin del ciclo while')'''
+    print('Fin del ciclo while')
 
-# Imprimir números del 0 al 5 con el ciclo while
-'''maximo =5
+# EJERCICIO 1 - Imprimir números del 0 al 5 con el ciclo while
+maximo =5
 contador = 0
 while contador <= maximo:
     print(contador)
     contador += 1'''
-
-'''minimo = 1
+'''
+#EJERCICIO 2
+minimo = 1
 contador = 5
 while contador>= minimo:
     print(contador)
-    contador -= 1'''
+    contador -= 1
 
-#Ciclo for
-'''cadena = 'Hello'
+
+# Ciclo for
+cadena = 'Hello'
 for letra in cadena:
     print(letra)
 else:
-    print('Fin del ciclo for')'''
-
+    print('Fin del ciclo for')
+'''
 #Palabra reservada break
 '''for letra in 'Alemania':
     if letra == 'a':
         print(f'Letra encontrada: {letra}')
         break
 else:
-    print('Fin del ciclo for')'''
-
+    print('Fin del ciclo for')
+'''
 #Palabra reservada continue
-for i in range(6):
+'''for i in range(6):
     if i % 2 == 0:
         print(f'Valor: {i}')
 
@@ -43,3 +45,4 @@ for i in range(6):
     if i % 2 != 0:
         continue
     print(f'Valor: {i}')
+'''
