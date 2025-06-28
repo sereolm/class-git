b0826ea1a5d6db46ad09bfab843e03dcc2109d84
Serena Olmedo <serenadelvalle25@gmail.com>
2025-06-27 22:08:03 -0300

    Resolví conflictos y mantuve el .gitignore correcto y el JS resuelto

--- JavaScript/Leccion03/03-01-OperadoresJavaScript-UJS.js
+++ JavaScript/Leccion03/03-01-OperadoresJavaScript-UJS.js
@@ -1,46 +1,46 @@
-//Ejercicio para encontrar números pares e impares
-let parInpar =10;
-if(parInpar % 2 == 0){
-    console.log("Es un número PAR");
-}
-else{
-    console.log("Es un numero IMPAR");
-}
+// //Ejercicio para encontrar números pares e impares
+// let parInpar =1;
+// if(parInpar % 2 == 0){
+//     console.log("Es un número PAR");
+// }
+// else{
+//     console.log("Es un numero IMPAR");
+// }
 
-//Ejercicio: es mayor de edad
-let edad= 18, adulto =18;
-if( edad >= adulto ){
-	console.log('Usted es una persona adulta');
-}
-else{
-	console.log('Usted es una persona menor de edad');
-}
+// //Ejercicio: es mayor de edad
+// let edad= 18, adulto =18;
+// if( edad >= adulto ){
+// 	console.log('Usted es una persona adulta');
+// }
+// else{
+// 	console.log('Usted es una persona menor de edad');
+// }
 
-//Ejercicio: Dentro de un rango
-let dentroRango = 10;//Aqui vamos a ir cambiando el valor
-let valMin = 0, valMax =10;
-if(dentroRango >= valMin && dentroRango<= valMax){
-    console.log('Esta dentro del rango establecido') 
-}
-else{
-    console.log('Esta fuera del rango establecido')
-}
+// //Ejercicio: Dentro de un rango
+// let dentroRango = 10;//Aqui vamos a ir cambiando el valor
+// let valMin = 0, valMax =10;
+// if(dentroRango >= valMin && dentroRango<= valMax){
+//     console.log('Esta dentro del rango establecido') 
+// }
+// else{
+//     console.log('Esta fuera del rango establecido')
+// }
 
 //Ejercicio: Si el padre puede asistir al juego de su hijo
-let vacaciones = true, diaDescanso = false;
-if(vacaciones || diaDescanso){
-    console.log("El padre puede asistir al juego de su hijo")
-}
-else{
-    console.log("El padre NO puede asistir al juego de su hijo")
-}
+//let vacaciones = true, diaDescanso = false;
+//if(vacaciones || diaDescanso){
+//    console.log("El padre puede asistir al juego de su hijo")
+//}
+//else{
+//    console.log("El padre NO puede asistir al juego de su hijo")
+//}
 //Operador ternario
-let resultado2 = 3 > 2 ? "Verdadero":"Falso";
-console.log(resultado2)
+// let resultado2 = 3 > 2 ? "Verdadero":"Falso";
+// console.log(resultado2)
 
-let numero = 12;
-resultado2 = numero % 2 == 0 ? "Es un número PAR" : "Es un número IMPAR";
-console.log(resultado2)
+// let numero = 12;
+// resultado2 = numero % 2 == 0 ? "Es un número PAR" : "Es un número IMPAR";
+// console.log(resultado2)
 
 // Convertir String a Number
 let miNumero = "21"; // Es una cadena
--- JavaScript/Leccion04/04-01-Ejercicios.js
+++ JavaScript/Leccion04/04-01-Ejercicios.js
@@ -2,66 +2,145 @@
 //Ejercicio 2: Hora del día
 
 //Ejercicio1
-let mes= 9;
-let estacion;
-if(mes == 1 || mes == 2 || mes ==12){
-    estacion = "Verano";
-}
-else if(mes == 3 || mes == 4 || mes == 5){
-    estacion= "Otoño";
-}
-else if(mes == 6 || mes == 7 || mes == 8){
-    estacion= "Invierno";
-}
-else if(mes == 9 || mes == 10 || mes == 11){
-    estacion= "Primavera";
-}
-else{
-    estacion = "Valor incorrecto";
-}
-console.log("El valor corresponde a la estación de: "+estacion);
+// let mes= 9;
+// let estacion;
+// if(mes == 1 || mes == 2 || mes ==12){
+//     estacion = "Verano";
+// }
+// else if(mes == 3 || mes == 4 || mes == 5){
+//     estacion= "Otoño";
+// }
+// else if(mes == 6 || mes == 7 || mes == 8){
+//     estacion= "Invierno";
+// }
+// else if(mes == 9 || mes == 10 || mes == 11){
+//     estacion= "Primavera";
+// }
+// else{
+//     estacion = "Valor incorrecto";
+// }
+// console.log("El valor corresponde a la estación de: "+estacion);
 
 //Ejercicio2
-//de 6 a 11 nos saluda: Good morning
-//de 12 a 16 nos saluda:good afternoom
-//de 17 a 19 nos saluda: good evening
-//de 20 1 23 nos saluda: good nigth
-//trabajaremos con 24 hs
+// de 6 a 11 nos saluda: Good morning
+// de 12 a 16 nos saluda:good afternoom
+// de 17 a 19 nos saluda: good evening
+// de 20 1 23 nos saluda: good nigth
+// trabajaremos con 24 hs
 
-let horaDia = 7;
-let mensaje;
-if(horaDia>= 6 && horaDia<=11){
-    mensaje = "Good Morning";
-}
-else if(horaDia>= 12 && horaDia<=16){
-    mensaje = "Good Afternoom";
-}
-else if(horaDia>= 17 && horaDia<=19){
-    mensaje = "Good Evening";
-}
-else if(horaDia>= 20 && horaDia<=23){
-    mensaje = "Good Night";
-}
-else{
-    mensaje = "Valor incorrecto";
-}
-console.log(mensaje);
+// let horaDia = 7;
+// let mensaje;
+// if(horaDia>= 6 && horaDia<=11){
+//     mensaje = "Good Morning";
+// }
+// else if(horaDia>= 12 && horaDia<=16){
+//     mensaje = "Good Afternoom";
+// }
+// else if(horaDia>= 17 && horaDia<=19){
+//     mensaje = "Good Evening";
+// }
+// else if(horaDia>= 20 && horaDia<=23){
+//     mensaje = "Good Night";
+// }
+// else{
+//     mensaje = "Valor incorrecto";
+// }
+// console.log(mensaje);
 
 //Estructura swich(La sintaxis es igual a Java)
-switch(mes){//No solo pueden utilizar número, también cadenas
-    case 1: case 2: case 12:
-    estacion = "Verano";
+// switch(mes){//No solo pueden utilizar número, también cadenas
+//     case 1: case 2: case 12:
+//     estacion = "Verano";
+//     break;
+//     case 3: case 4: case 5:
+//     estacion = "Otoño";
+//     break;
+//     case 6: case 7: case 8:
+//     estacion = "Invierno";
+//     break;
+//     case 9: case 10: case 11:
+//     estacion = "Primavera";
+//     break;
+//     default:
+//         estacion = "Valor incorrecto";
+// }
+// console.log("Bienvenido a la estación de: "+estacion);
+
+// //Ampliando el uso de var let y const
+// */
+// //Con var puedes resignar en cualquier momento
+// //este forma parte del ambito global
+// //un error es que se sobreescriba
+
+// var nombre = 'Ariel';
+// nombre = 'Osvaldo';
+// console.log(nombre);
+
+// function saludar() {
+//     var nombre = 'Natalia';
+//     console.log(nombre); // Aquí no lee el dato externo, usa el interno de la función
+// }
+
+// console.log(nombre); // Muestra 'Osvaldo'
+// saludar();           // Muestra 'Natalia'
+
+// if (true) {
+//     var edad = 34;
+//     console.log(edad);
+// }
+
+// console.log(edad); // En la función funcionó correctamente, en la estructura if no falló
+
+// //const se ituliza para valores constantes que no pueden ser reasignadas
+
+// const fechaNacimiento = 2006;
+// console.log(fechaNacimiento);
+//fechaNacimiento = 2003;
+//console.log(fechaNacimiento); //solo se ejecuta el console anterior
+//Let days =["Lunes","Martes","Miercoles","Jueves","Viernes","Sabado","Domingo"];
+
+//function getDay(n)
+
+//Evitar repetir tu codigo
+//Dry don´t repeat yourself
+let days = 5;
+switch (days) {
+  case 1:
+    console.log('hoy es Lunes');
     break;
-    case 3: case 4: case 5:
-    estacion = "Otoño";
+  case 2:
+    console.log('hoy es Martes');
     break;
-    case 6: case 7: case 8:
-    estacion = "Invierno";
+  case 3:
+    console.log('hoy es Miercoles');
     break;
-    case 9: case 10: case 11:
-    estacion = "Primavera";
+  case 4:
+    console.log('hoy es Jueves');
+    break;
+  case 5:
+    console.log('hoy es Viernes');
+    break;
+  case 6:
+    console.log('hoy es Sábado');
+    break;
+    case 7:
+    console.log('hoy es Domingo');
     break;
     default:
-        estacion = "Valor incorrecto";
+        console.log("Error en el ingreso del día de la semana")
 }
+
 console.log("Bienvenido a la estación de: "+estacion);
+
+
+//Esta es la version mejorada
+
+let days2 = ["Lunes","Martes","Miercoles","Jueves","Viernes","Sabado","Domingo"];
+function getDay(n){
+    if(n < 1 || n > 7){
+        throw new Error("out of range");
+    }
+    return days2[n-1];
+}
+console.log(getDay(3));
+
