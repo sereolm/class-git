42ade80c37bb4bb59f882955ac246b209f015feb
Serena Olmedo <serenadelvalle25@gmail.com>
2025-08-18 16:24:10 -0300

    Cree la carpeta para los archivos del segundo semestre, y la carpeta para cada materia

---
+++ SEGUNDO SEMESTRE/PROGRAMACION 2 LUNES/PYTHON/CLASE 1 dia 11 de agosto/Leccion4/main.py
@@ -0,0 +1,7 @@
+# lista = Ariel, Liliana, Natalia, Osvaldo
+
+nombres = ['Naty','Osvaldo', 'Lily','Ariel']
+print(nombres)
+print(nombres[0:2]) #Solo muestra el indice 0,1 pero no el indice 2
+#ir del inicio de la lista al indice (sin incluirlo)
+print(nombres[ :3])
